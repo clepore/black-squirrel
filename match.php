3b4af2083ea5f3f859ab195d8761c88e8e594ce6
Christopher Lepore <christopher.r.lepore@gmail.com>
2013-07-08 01:16:22 -0400

    Write to output file

--- match.php
+++ match.php
@@ -2,14 +2,10 @@
 
 <?php
 
-if ($argc != 3 || in_array($argv[1], array('--help', '-help', '-h', '-?'))) {
-  echo "REQUIRES 2 INPUTS!!!\n";
-  die();
-}
-
 $lines = split("\n", file_get_contents($argv[1], "r"));
 $patterns = array();
 $tests = array();
+$out = array();
 
 // Separate the patterns indexed by length
 $p_len = (int) $lines[0];
@@ -18,31 +14,31 @@ for ($i=1; $i <= $p_len; $i++) {
   $patterns[count($exp)][] = $lines[$i];
 }
 
+// Sort the patterns based on specificity
 foreach ($patterns  as &$p) {
   usort(&$p, 'cmp');
 }
 
-// Separate the tests
+// Separate and trim the test strings
 $t_start = $p_len + 1;
 $t_len = (int) $lines[$t_start];
-
 for ($i=$t_start+1; $i <= count($lines)-1; $i++) {
   $tests[] = trim($lines[$i], '/');
 }
 
+// Loop through each test
 foreach ($tests as $test) {
 
-  $result = "\n".$test." = NO MATCH";
-
+  $result = 'NO MATCH';
   $pieces = explode('/', $test);
-
   $len = count($pieces);
 
+  // Compare test with patterns that match its length
   if (isset($patterns[$len])) {
 
-    $arr = $patterns[$len];
+    $pat = $patterns[$len];
 
-    foreach ($arr as $p) {
+    foreach ($pat as $p) {
       $p_arr = explode(',',$p);
 
       for ($i = 0; $i < $len; $i++) {
@@ -53,33 +49,38 @@ foreach ($tests as $test) {
       }
 
       if ($i === $len) {
-        $result = "\n".$test.' = ' .$p;
+        $result = $p;
         break;
       } 
     }
   }
-  echo $result;
+  $out[] = $result;
 }
 
+// Write contents to output file
+file_put_contents($argv[2], implode("\n", $out));
+
+
+
+/**
+ * Pattern Sort Function
+ */
 function cmp($a, $b) {
   $amtA = substr_count($a, '*');
   $amtB = substr_count($b, '*');
 
-  if ($amtA === $amtB) { // Have the same number of *'s
+  if ($amtA === $amtB) { // Have the same amount of *'s
     $posA = strpos($a, '*');
     $posB = strpos($b, '*');
 
-    if ($posA === $posB) { // Have the * in the same position
+    if ($posA === $posB) { // Have the same index for *
       return cmp(substr($a, $posA + 1), substr($b, $posB + 1));
-    
     } else {
       return ($posA > $posB) ? -1 : 1;
     }
-  
   } else {
     return ($amtA < $amtB) ? -1 : 1;
   }
 }
-
 
 ?>
